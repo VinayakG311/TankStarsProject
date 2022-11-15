a0ead4c6a1ba59265b748f6004fce4bcb825689f
VinayakG311 <vinayak21113@iiitd.ac.in>
2022-11-15 20:54:49 +0530

    made changes

--- core/src/com/mygdx/game/Screens/PlayScreen.java
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -6,7 +6,12 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -17,17 +22,45 @@ import com.mygdx.game.tankStars;
 public class PlayScreen implements Screen {
     private tankStars game;
     private final Texture backGround;
+    private final Texture ground;
     private OrthographicCamera gamecam;
     private Viewport gamePort;
+    private ShapeRenderer shapeRenderer;
     private hud hud;
+    private BitmapFont bf_loadProgress;
+    private TmxMapLoader maploader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
 
     public PlayScreen(tankStars game){
         this.game=game;
         gamecam=new OrthographicCamera();
         gamePort=new ExtendViewport(850,500,gamecam);
         backGround = new Texture("background.jpg");
-        gamecam.position.set(gamePort.getWorldWidth()/2,gamePort.getWorldHeight()/2,0);
+        ground=new Texture("map.png");
+
         hud = new hud(game.sprite);
+        bf_loadProgress = new BitmapFont();
+        bf_loadProgress.getData().setScale(2,1);
+        shapeRenderer = new ShapeRenderer();
+//        maploader=new TmxMapLoader();
+//        map=maploader.load("map.jpg");
+//        renderer=new OrthogonalTiledMapRenderer(map);
+//        gamecam.position.set(gamePort.getWorldWidth()/2,gamePort.getWorldHeight()/2,0);
+
+
+    }
+    public void handleinput(float dt){
+        if(Gdx.input.isTouched()){
+            gamecam.position.x+=100*dt;
+        }
+
+    }
+    public void update(float dt){
+        handleinput(dt);
+        gamecam.update();
+      //  renderer.setView(gamecam);
 
     }
     @Override
@@ -37,15 +70,29 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        update(delta);
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        //game.sprite.setProjectionMatrix(gamecam.combined);
+       // renderer.render();
+        //   game.sprite.setProjectionMatrix(gamecam.combined);
         game.sprite.begin();
         game.sprite.draw(backGround,0,0,850,500);
+        game.sprite.draw(ground,0,0,850,100);
         game.sprite.end();
+        showHealth();
         game.sprite.setProjectionMatrix(hud.stage.getCamera().combined);
-        hud.stage.draw();
-        hud.stage.act();
+
+
+    }
+    public void showHealth(){
+       
+        float health = 175;
+        shapeRenderer.setProjectionMatrix(gamecam.combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.RED);
+        shapeRenderer.rect(-260, 225, health, 30);
+        shapeRenderer.rect(150, 225, health, 30);
+        shapeRenderer.end();
 
     }
 
