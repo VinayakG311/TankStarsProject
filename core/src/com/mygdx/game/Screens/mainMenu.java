f328b387c7b86bbc6a320f0b3ccd60659c9ce230
vinayak-2021112 <vinayak21112@iiitd.ac.in>
2022-11-19 13:48:39 +0530

    Menu Styling done

--- core/src/com/mygdx/game/Screens/mainMenu.java
+++ core/src/com/mygdx/game/Screens/mainMenu.java
@@ -11,12 +11,12 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.game.tankStars;
 
 public class mainMenu extends ScreenAdapter implements Screen {
@@ -36,6 +36,11 @@ public class mainMenu extends ScreenAdapter implements Screen {
     public Skin skin;
 
 
+    private Label outputLabel;
+
+
+
+
 
     public mainMenu(final tankStars game) {
         super();
@@ -51,61 +56,107 @@ public class mainMenu extends ScreenAdapter implements Screen {
         TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
         textButtonStyle.font= new BitmapFont();
 
-        Button button = new TextButton("Play",textButtonStyle);
-        button.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                System.out.println("hi");
-                game.setScreen(new PlayScreen(game));
-            }
-        });
-
-        button.setBounds(50,50,25,25);
-        stage.addActor(button);
 
     }
-
-
-
 
 
     @Override
-    public void show() {
-        sb = new SpriteBatch();
+    public void show () {
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
         Texture img = new Texture("background.png");
-        sprite = new Sprite(img);
-        Texture newGame = new Texture("newGame.png");
-        sprite2 = new Sprite(newGame);
-        sprite.setSize(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
 
-        sprite2.setBounds(Gdx.graphics.getWidth()/2-125,Gdx.graphics.getHeight()/2,250,40 );
 
-//        newGame = new Texture("newGame.png");
-//        ImageButton button1 = new ImageButton((Drawable) newGame);
-//        button1.setBounds(Gdx.graphics.getWidth()/2-125,Gdx.graphics.getHeight()/2,250,40);
-//        button1.addListener(new InputListener(){
-//            @Override
-//            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-//                game.setScreen(new PlayScreen(game));
-//            }
-//            @Override
-//            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-//                game.setScreen(new PlayScreen(game));
-//                return true;
-//            }
-//        });
-//        stage.addActor(button1);
-//        Gdx.input.setInputProcessor(new InputAdapter() {
-//            @Override
-//            public boolean keyDown(int keyCode) {
-//                if (keyCode == Input.Keys.SPACE) {
-//                    game.setScreen(new PlayScreen(game));
-//                }
-//                return true;
-//            }
-//        });
+        int Help_Guides = 12;
+        int row_height = Gdx.graphics.getWidth() / 12;
+        int col_width = Gdx.graphics.getWidth() / 12;
+
+        Skin mySkin = new Skin(Gdx.files.internal("skin/star-soldier-ui.json"));
+
+        Label title = new Label("Tank Stars",mySkin);
+        title.setSize(Gdx.graphics.getWidth(),row_height*2);
+        title.setPosition(0,Gdx.graphics.getHeight()-row_height*2);
+        title.setAlignment(Align.center);
+        stage.addActor(title);
+
+        // Button
+        Button button1 = new TextButton("NEW GAME",mySkin);
+        button1.setSize(col_width*8,row_height*2);
+        button1.setPosition(100,250);
+        button1.addListener(new ClickListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new PlayScreen(game));
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new PlayScreen(game));
+                return true;
+            }
+        });
+        stage.addActor(button1);
+
+        // Text Button
+        Button button2 = new TextButton("CONTINUE",mySkin);
+        button2.setSize(col_width*8,row_height*2);
+        button2.setPosition(100,125);
+        button2.addListener(new ClickListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new PlayScreen(game));
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new PlayScreen(game));
+                return true;
+            }
+        });
+        stage.addActor(button2);
+
     }
+
+
+
+
+
+//    @Override
+//    public void show() {
+//        sb = new SpriteBatch();
+//        Texture img = new Texture("background.png");
+//        sprite = new Sprite(img);
+//        Texture newGame = new Texture("newGame.png");
+//        sprite2 = new Sprite(newGame);
+//        sprite.setSize(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+//
+//
+//        sprite2.setBounds(Gdx.graphics.getWidth()/2-125,Gdx.graphics.getHeight()/2,250,40 );
+//
+////        newGame = new Texture("newGame.png");
+////        ImageButton button1 = new ImageButton((Drawable) newGame);
+////        button1.setBounds(Gdx.graphics.getWidth()/2-125,Gdx.graphics.getHeight()/2,250,40);
+////        button1.addListener(new InputListener(){
+////            @Override
+////            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+////                game.setScreen(new PlayScreen(game));
+////            }
+////            @Override
+////            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+////                game.setScreen(new PlayScreen(game));
+////                return true;
+////            }
+////        });
+////        stage.addActor(button1);
+////        Gdx.input.setInputProcessor(new InputAdapter() {
+////            @Override
+////            public boolean keyDown(int keyCode) {
+////                if (keyCode == Input.Keys.SPACE) {
+////                    game.setScreen(new PlayScreen(game));
+////                }
+////                return true;
+////            }
+////        });
+//    }
 
 
 
@@ -114,10 +165,16 @@ public class mainMenu extends ScreenAdapter implements Screen {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        sb.begin();
-        sprite.draw(sb);
-        //sprite2.draw(sb);
-        sb.end();
+//        sb.begin();
+//        sprite.draw(sb);
+//        //sprite2.draw(sb);
+//        sb.end();
+//        stage.draw();
+
+        stage.act();
+        stage.getBatch().begin();
+        stage.getBatch().draw(img,0,0,850,500);
+        stage.getBatch().end();
         stage.draw();
     }
 
