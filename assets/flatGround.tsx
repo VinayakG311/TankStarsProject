b76083380cc92ad652adbd771cf55233048f27f8
VinayakG311 <vinayak21113@iiitd.ac.in>
2022-11-23 22:17:44 +0530

    map

---
+++ assets/flatGround.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="flatGround" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="544" columns="68">
+ <image source="flatGround.png" width="1224" height="160"/>
+</tileset>
---
+++ core/src/com/mygdx/game/MapScreen.java
@@ -0,0 +1,122 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+
+public class MapScreen implements Screen {
+    tankStars game;
+    World world;
+    Body body;
+    Body b2body;
+    Box2DDebugRenderer box2DDebugRenderer;
+
+
+    TiledMap tiledMap;
+    OrthogonalTiledMapRenderer renderer;
+    OrthographicCamera camera;
+    TmxMapLoader loader;
+    MapScreen(tankStars game){
+        this.game=game;
+        camera=new OrthographicCamera();
+        camera.setToOrtho(false,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        camera.zoom+=0.5;
+        //camera.update();
+        tiledMap= new TmxMapLoader().load("newMap.tmx");
+        renderer = new OrthogonalTiledMapRenderer(tiledMap);
+        world=new World(new Vector2(0,-10),true);
+        deftank();
+        defground();
+        box2DDebugRenderer=new Box2DDebugRenderer();
+
+
+
+    }
+    @Override
+    public void show() {
+
+    }
+    public void deftank(){
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(50,100);
+
+        bdef.type=BodyDef.BodyType.DynamicBody;
+        body = world.createBody(bdef);
+        FixtureDef fdef = new FixtureDef();
+        fdef.density=2.5f;
+        fdef.friction = 0.25f;
+        fdef.restitution=0.75f;
+        CircleShape shape = new CircleShape();
+        shape.setRadius(10f);
+        fdef.shape=shape;
+        body.createFixture(fdef);
+    }
+    public void defground(){
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(0,0);
+        bdef.type=BodyDef.BodyType.StaticBody;
+        b2body=world.createBody(bdef);
+        FixtureDef fdef = new FixtureDef();
+        fdef.density=2.5f;
+        fdef.friction = 0.5f;
+        fdef.restitution=0;
+
+        ChainShape shape = new ChainShape();
+
+        shape.createChain(new Vector2[]{new Vector2(-500,-1.7f),new Vector2(500,-1.7f)});
+        shape.setRadius(0.1f);
+        fdef.shape=shape;
+        b2body.createFixture(fdef);
+
+    }
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.sprite.begin();
+        game.sprite.draw(new Texture("background.png"),0,0);
+        game.sprite.end();
+        if(Gdx.input.isTouched()){
+            camera.translate(Gdx.input.getDeltaX(),Gdx.input.getDeltaY());
+            camera.update();
+        }
+
+        renderer.setView(camera);
+        renderer.render();
+        box2DDebugRenderer.render(world,camera.combined);
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
