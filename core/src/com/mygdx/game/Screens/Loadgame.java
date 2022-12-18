26e04806e54d5f5840ed701c8e721ceb0cf03a31
VinayakG311 <vinayak21113@iiitd.ac.in>
2022-12-18 10:42:34 +0530

    loadscreen

--- core/src/com/mygdx/game/Screens/Loadgame.java
+++ core/src/com/mygdx/game/Screens/Loadgame.java
@@ -1,26 +1,61 @@
 package com.mygdx.game.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.mygdx.game.tankStars;
 
 public class Loadgame  implements Screen {
+
+    private Preferences preferences;
     private tankStars game;
     private Stage stage;
+    private ImageButton load;
+    private Texture cont;
     private Texture img;
     public Loadgame(tankStars game){
+        preferences= Gdx.app.getPreferences("saveload");
+        cont=new Texture("bullet.png");
         stage=new Stage();
         this.game=game;
         Gdx.input.setInputProcessor(stage);
         img = new Texture("background.png");
 
+
     }
     @Override
     public void show() {
-        
+        Drawable drawable1= new TextureRegionDrawable(cont);
+        Gdx.input.setInputProcessor(stage);
+        load=new ImageButton(drawable1);
+        load.setSize(45,45);
+        load.setPosition(45,500);
+        load.addListener(new ClickListener(){
+
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("hi");
+//                saveload.setstate(getplayer(),1);
+//                saveload.setstate(getplayer2(),2);
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                //    saveload.setstate(trialMapScreen.this);
+                System.out.println("hi");
+                return true;
+            }
+        });
+
+        stage.addActor(load);
+
 
     }
 
@@ -28,13 +63,11 @@ public class Loadgame  implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         stage.act();
         stage.getBatch().begin();
         stage.getBatch().draw(img,0,0,1600,620);
         stage.getBatch().end();
         stage.draw();
-
     }
 
     @Override
