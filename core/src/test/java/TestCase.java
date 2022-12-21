575581049cfd133e7f260d21746a3b16892dd3ce
VinayakG311 <vinayak21113@iiitd.ac.in>
2022-12-21 19:50:45 +0530

    junit

---
+++ core/src/test/java/TestCase.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestCase {
+    @Test
+    public void TestCase1(){
+        int a=5;
+        int b=a+3;
+        assertEquals(b,8);
+    }
+
+}
